49fe4c9bf7c8aa4b2d603df33137c5bba302fa30
nbhatawdekar <autocad7>
2016-03-02 22:36:55 +0530

    2016-17

--- data/expenditure_by_ministry.js
+++ data/expenditure_by_ministry.js
@@ -1,163 +1,162 @@
 var finance_data = [
-  ["Department","Expenditure (2015-16)"],
-  ["Department of Economic Affairs",17239.56],
-  ["Department of Financial Services",24866.8],
-  ["Interest Payments",456145.05],
-  ["Transfers to State and Union Territory Governments",125189.52],
-  ["Department of Expenditure",156.84],
-  ["Pensions",26285],
-  ["Indian Audit and Accounts Department",3429.05],
-  ["Department of Revenue",15709],
-  ["Direct Taxes",5406.6],
-  ["Indirect Taxes",5664.6],
-  ["Department of Disinvestment",44]
+  ["Department","Expenditure (2016-17)"],
+  ["Department of Economic Affairs",16046],
+  ["Department of Financial Services",31976],
+  ["Interest Payments",492670],
+  ["Transfers to State and Union Territory Governments",125496],
+  ["Department of Expenditure",227],
+  ["Pensions",31070],
+  ["Indian Audit and Accounts Department",3665],
+  ["Department of Revenue",11544],
+  ["Direct Taxes",5387],
+  ["Indirect Taxes",5340],
+  ["Department of Disinvestment",40]
 ];
 
 var defence_data = [
-  ["Department","Expenditure (2015-16)"],
-  ["Defence Pensions",54500],
-  ["Defence Services - Army",104158.95],
-  ["Defence Services - Navy",15525.64],
-  ["Defence Services - Air Force",23000.09],
-  ["Defence Ordnance Factories",2884.23],
-  ["Defence Services - R & D",6570.09],
-  ["Capital Outlay on Defence Services",94588]
+  ["Department","Expenditure (2016-17)"],
+  ["Defence Pensions",82333],
+  ["Misc.",36133],
+  ["Defence Services (Revenue)",143869],
+  ["Capital Outlay on Defence",78587]
 ];
 
 var consumer_affairs_data = [
-  ["Department","Expenditure (2015-16)"],
-  ["Department of Consumer Affairs",261.77],
-  ["Department of Food and Public Distribution",125212.00]
+  ["Department","Expenditure (2016-17)"],
+  ["Department of Consumer Affairs",1242],
+  ["Department of Food and Public Distribution",140150]
 ];
 
 
 var chemicals_data = [
-  ["Department","Expenditure (2015-16)"],
-  ["Department of Chemicals and Petrochemicals",255.18],
-  ["Department of Fertilisers",73047.80],
-      ["Department of Pharmaceuticals",259.02]
+  ["Department","Expenditure (2016-17)"],
+  ["Department of Chemicals and Petrochemicals",202],
+  ["Department of Fertilisers",70039],
+  ["Department of Pharmaceuticals",211]
 ];
 
 var rural_development_data = [
-  ["Department","Expenditure (2015-16)"],
-  ["Department of Rural Development",71695.08],
-  ["Department of Land Resources",1637.50]
+  ["Department","Expenditure (2016-17)"],
+  ["Department of Rural Development",86056],
+  ["Department of Land Resources",1709]
 ];
 
 var hrd_data = [
-  ["Department","Expenditure (2015-16)"],
-  ["Department of School Education and Literacy",42219.50],
-  ["Department of Higher Education",26855.26]
+  ["Department","Expenditure (2016-17)"],
+  ["Department of School Education and Literacy",43554],
+  ["Department of Higher Education",28840]
 ]
 
 var home_affairs_data = [
-  ["Department","Expenditure (2015-16)"],
-  ["Ministry",1570],
-  ["Cabinet",416.99],
-  ["Police",62124.52],
-  ["Other Expenditure",2564.59],
-  ["Transfers to Union Territory Governments",1768.00]
+  ["Department","Expenditure (2016-17)"],
+  ["Ministry",4631],
+  ["Cabinet",420],
+  ["Police",70185],
+  ["Transfers to Union Territory Governments",2148]
 
 ]
 
 var road_transport_data = [
-  ["Department","Expenditure (2015-16)"],
-  ["Ministry",45751.65]
+  ["Department","Expenditure (2016-17)"],
+  ["Ministry",57976]
 ]
 
 var railway_data = [
-  ["Department","Expenditure (2015-16)"],
-  ["Ministry",40000]
+  ["Department","Expenditure (2016-17)"],
+  ["Ministry",45000]
 ]
 
 var health_data = [
-  ["Department","Expenditure (2015-16)"],
-  ["Department of Health and Family Welfare",29653],
-  ["Department of Health Research",1018.17],
-  ["Department of AIDS Control",1397]
+  ["Department","Expenditure (2016-17)"],
+  ["Department of Health and Family Welfare",37062],
+  ["Department of Health Research",1145]
 
 ];
-
-var it_data = [
-  ["Department","Expenditure (2015-16)"],
-  ["Department of Posts",7138.70],
-  ["Department of Telecommunications",13474.60],
-  ["Department of Electronics and Information Technology",2630]
-
-];
-
-var urban_dev_data = [
-  ["Department","Expenditure (2015-16)"],
-  ["Department of Urban Development",16832.20],
-  ["Department of Public Works",2326.98],
-  ["Stationery & Printing ",57.69]
-
-];
-
-var external_affairs_data = [
-  ["Department","Expenditure (2015-16)"],
-  ["Ministry",14966.83]
-
-];
-
-
-var petroleum_data = [
-  ["Department","Expenditure (2015-16)"],
-  ["Ministry",30125.55]
-];
-
-var agriculture_data = [
-  ["Department","Expenditure (2015-16)"],
-  ["Department of Agriculture and Cooperation",17004.35],
-  ["Department of Agriculture Research and Education",6320],
-  ["Department of Animal Husbandry, Dairying and Fisheries",1585.43]
-
-];
+//
+//var it_data = [
+//  ["Department","Expenditure (2016-17)"],
+//  ["Department of Posts",7138.70],
+//  ["Department of Telecommunications",13474.60],
+//  ["Department of Electronics and Information Technology",2630]
+//
+//];
+//
+//var urban_dev_data = [
+//  ["Department","Expenditure (2016-17)"],
+//  ["Department of Urban Development",16832.20],
+//  ["Department of Public Works",2326.98],
+//  ["Stationery & Printing ",57.69]
+//
+//];
+//
+//var external_affairs_data = [
+//  ["Department","Expenditure (2016-17)"],
+//  ["Ministry",14966.83]
+//
+//];
+//
+//
+//var petroleum_data = [
+//  ["Department","Expenditure (2016-17)"],
+//  ["Ministry",30125.55]
+//];
+//
+//var agriculture_data = [
+//  ["Department","Expenditure (2016-17)"],
+//  ["Department of Agriculture and Cooperation",17004.35],
+//  ["Department of Agriculture Research and Education",6320],
+//  ["Department of Animal Husbandry, Dairying and Fisheries",1585.43]
+//
+//];
 
 var others_data = [
-  ["Department","Expenditure (2015-16)"],
-  ["Department of Atomic Energy",10912.00],
-  ["Ministry of Women and Child Development",10382.40],
-  ["Union Territories without Legislature",10026.25],
-  ["Ministry of Science and Technology",9491.84],
-  ["Ministry of Commerce and Industry",7707.70],
-  ["Department of Space",7388.19],
-  ["Ministry of Social Justice and Empowerment",7161.76],
-  ["Ministry of Power",6725.93],
-  ["Ministry of Drinking Water and Sanitation",6243.87],
-  ["Ministry of Housing and Urban Poverty Alleviation",5634.47],
-  ["Ministry of Labour and Employment",5361.37],
-  ["Ministry of Statistics and Programme Implementation",4826.87],
-  ["Ministry of Tribal Affairs",4819.21],
-  ["Ministry of Textiles",4274.80],
-  ["Ministry of Water Resources, River Development and Ganga Rejuvenation",4232.43],
-  ["Ministry of Law and Justice",3758.65],
-  ["Ministry of Minority Affairs",3738.11],
-  ["Ministry of Information and Broadcasting",3711.11],
-  ["Ministry of Civil Aviation",3341.50],
-  ["Ministry of Micro, Small and Medium Enterprises",3007.42],
-  ["Ministry of Development of North Eastern Region",2362.74],
-  ["Ministry of Planning",2211.63],
-  ["Ministry of Culture",2169.00],
-  ["Ministry of Environment, Forest and Climate Change",1681.6],
-  ["Ministry of Heavy Industries and Public Enterprises",1643.73],
-  ["Ministry of Earth Sciences",1619.7],
-  ["Ministry of Tourism",1573.07],
-  ["Ministry of Skill Development and Entrepreneurship",1543.46],
-  ["Ministry of Youth Affairs and Sports",1541.13],
-  ["Ministry of Shipping",1439.79],
-  ["Ministry of AYUSH",1214.00],
-  ["The President, Parliament, UPSC and Secretariat of Vice President",1186.04],
-  ["Ministry of Personnel, Public Grievances and Pensions",1181.58],
-  ["Ministry of Mines",1155.71],
-  ["Ministry of Coal",606.00],
-  ["Ministry of Food Processing Industries",505.51],
-  ["Ministry of New and Renewable Energy",303.21],
-  ["Ministry of Corporate Affairs",271.88],
-  ["Ministry of Overseas Indian Affairs",118.17],
-  ["Ministry of Panchayati Raj",94.75],
-  ["Ministry of Steel",82.95],
-  ["Ministry of Parliamentary Affairs",15.57]
+  ["Department","Expenditure (2016-17)"],
+  ["Ministry of Petroleum and Natural Gas",29161],
+  ["Ministry of Agriculture and Farmers Welfare",44485],
+  ["Ministry of Communications and Information Technology",30968],
+  ["Ministry of Urban Development",24523],
+  ["Ministry of External Affairs",14663],
+  ["Department of Atomic Energy",11682],
+  ["Ministry of Women and Child Development",17408],
+  ["Union Territories without Legislature",10623],
+  ["Ministry of Science and Technology",10353],
+  ["Ministry of Commerce and Industry",7389],
+  ["Department of Space",7509],
+  ["Ministry of Social Justice and Empowerment",7349],
+  ["Ministry of Power",12253],
+  ["Ministry of Drinking Water and Sanitation",14010],
+  ["Ministry of Housing and Urban Poverty Alleviation",5411],
+  ["Ministry of Labour and Employment",6242],
+  ["Ministry of Statistics and Programme Implementation",4753],
+  ["Ministry of Tribal Affairs",4826],
+  ["Ministry of Textiles",4595],
+  ["Ministry of Water Resources, River Development and Ganga Rejuvenation",6201],
+  ["Ministry of Law and Justice",5411],
+  ["Ministry of Minority Affairs",3827],
+  ["Ministry of Information and Broadcasting",4084],
+  ["Ministry of Civil Aviation",2591],
+  ["Ministry of Micro, Small and Medium Enterprises",3465],
+  ["Ministry of Development of North Eastern Region",2430],
+  ["Ministry of Planning",293],
+  ["Ministry of Culture",2500],
+  ["Ministry of Environment, Forest and Climate Change",2250],
+  ["Ministry of Heavy Industries and Public Enterprises",1320],
+  ["Ministry of Earth Sciences",1672],
+  ["Ministry of Tourism",1590],
+  ["Ministry of Skill Development and Entrepreneurship",1804],
+  ["Ministry of Youth Affairs and Sports",1592],
+  ["Ministry of Shipping",1531],
+  ["Ministry of AYUSH",1326.20],
+  ["The President, Parliament, UPSC and Secretariat of Vice President",1275.70],
+  ["Ministry of Personnel, Public Grievances and Pensions",1478],
+  ["Ministry of Mines",1214],
+  ["Ministry of Coal",361],
+  ["Ministry of Food Processing Industries",636.02],
+  ["Ministry of New and Renewable Energy",5036],
+  ["Ministry of Corporate Affairs",344],
+  ["Ministry of Panchayati Raj",768],
+  ["Ministry of Steel",86],
+  ["Ministry of Parliamentary Affairs",17.3]
 ]
 
 
